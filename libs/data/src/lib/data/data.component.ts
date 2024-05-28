11bea3727882942db8add612d19cf5d8992d2c0d
PC-DAVID <david.lopez.enggmail.com>
2024-05-28 16:43:59 -0500

    Adds data and logic layer

---
+++ libs/data/src/lib/data/data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-data',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data.component.html',
+  styleUrl: './data.component.css',
+})
+export class DataComponent {}
