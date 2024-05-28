1b99dd54fbe0d5aaecd38908267239c82793ee63
PC-DAVID <david.lopez.enggmail.com>
2024-05-28 17:24:17 -0500

    Adds logic library

---
+++ libs/logic/src/lib/logic/logic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-logic',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './logic.component.html',
+  styleUrl: './logic.component.css',
+})
+export class LogicComponent {}
