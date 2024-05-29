cfda2e00b6f87680ae81cb4e5af97179b90ed9cf
PC-DAVID <david.lopez.enggmail.com>
2024-05-29 00:24:06 -0500

    Refactor in app files, moved to ui lib

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,1 +1,2 @@
 export const COINK_UI_LAYER = 'COINK_UI_LAYER';
+export * from './app';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/component';
+import { AppComponent } from '@coink-app/ui';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
